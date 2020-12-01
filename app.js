6aed3bdb44ea54ae315dd1a814df0f8ce9075d24
jmsaylor <johnmsaylor@gmail.com>
2020-11-30 19:35:35 -0500

    working gateway w/ 2 actions

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+const key = process.env.NEWS_KEY;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+app.get("/monitor", (req, res) => {
+  console.log("hello");
+  res.send();
+});
+
+app.post("/", async (req, res) => {
+  const { action, data } = req.body;
+
+  switch (action) {
+    case "searchTerm":
+      try {
+        res.send(await searchNewsArticles(data));
+      } catch (error) {
+        res.status(500);
+        console.error(error.message);
+      }
+      break;
+    case "topHeadlines":
+      try {
+        res.send(await getTopHeadlines());
+      } catch (error) {
+        console.error(error);
+        res.status(500);
+      }
+      break;
+    default:
+      res.send({ message: "Invalid action type" });
+      break;
+  }
+});
+
+const newsURI = "http://newsapi.org/v2/";
+
+const searchNewsArticles = async (query) => {
+  const date = "2020-11-29";
+  const url =
+    newsURI +
+    `everything?q=${query}&from=${date}&sortBy=popularity&apiKey=${key}`;
+
+  try {
+    const articles = await fetch(url);
+    return await articles.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getTopHeadlines = async () => {
+  const url = newsURI + `top-headlines?country=us&apiKey=${key}`;
+
+  try {
+    const articles = await fetch(url);
+    return await articles.json();
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
